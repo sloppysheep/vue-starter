a7751ecad3356442978b6c786b5e6de1310d0294
Aleksandra Wandzilak <aleksandra.wandzilak@gmail.com>
2020-04-25 15:47:01 +0200

    computed instead of mounted

--- src/LoginForm.vue
+++ src/LoginForm.vue
@@ -2,7 +2,7 @@
     <div>
         <label>Zaloguj się e-mailem</label>
         <input type="email" v-model="email">
-        <button @click="enter()">{{buttonLabel}}</button>
+        <button @click="enter()">{{buttonLabelToDisplay}}</button>
     </div>
 </template>
 
@@ -18,6 +18,16 @@
             enter() {
                 this.$emit('login', this.email);
             }
-        }
+        },
+        //mounted() {
+        //	  if (!this.buttonLabel) {
+        //	    this.buttonLabel = 'Zaloguj się';
+        //	  }
+        //	},
+        computed: {
+      	  buttonLabelToDisplay() {
+     		 return this.buttonLabel || 'Zaloguj się';  
+    		}
+      	}
     }
 </script>
